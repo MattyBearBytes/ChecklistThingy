00e09e22eefc6cde1b3e8a179907cc434a810f06
Matt <matt@thebearbytes.com.au>
2013-05-30 23:06:09 +0800

    Initial checkin

---
+++ source/ChecklistThingy/App_Start/AuthConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using ChecklistThingy.Models;
+
+namespace ChecklistThingy
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            OAuthWebSecurity.RegisterMicrosoftClient(
+                clientId: "asfd",
+                clientSecret: "asf");
+
+            OAuthWebSecurity.RegisterTwitterClient(
+                consumerKey: "asf",
+                consumerSecret: "asf");
+
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: "asfd",
+                appSecret: "asf");
+
+            OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
